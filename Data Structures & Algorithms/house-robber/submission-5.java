fa2ab04961b80fa203f66eb49c57f94b511fe976
Sushil Gupta <119423299+SGgithub001@users.noreply.github.com>
2026-05-08 14:26:36 +0530

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        return solve(nums, 0, dp);
+    }
+    public int solve(int[] nums, int idx, int[] dp) {
+        if (idx >= nums.length)
+            return 0;
+        if (dp[idx] != -1)
+            return dp[idx];
+        int steal = nums[idx] + solve(nums, idx + 2, dp);
+        int skip = solve(nums, idx + 1, dp);
+        dp[idx] = Math.max(steal, skip);
+        return dp[idx];
+    }
+}
